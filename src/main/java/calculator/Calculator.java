3794833ae0a9ab94874f9c5c425b8a20df1b7a7f
PPakSang <sanghyun-dev@naver.com>
2023-07-04 01:30:25 +0900

    feat: 입력값이 null 일 때 결과값을 반환하는 코드 추가
    Co-authored-by: dahyen0o <seeunchoi99@gmail.com>
    Co-authored-by: PPakSang <sanghyun-dev@naver.com>

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -25,7 +25,7 @@ public class Calculator {
     }
 
     public static int sum(String text) {
-        if (text.isEmpty()) {
+        if (text == null || text.isEmpty()) {
             return 0;
         }
 
